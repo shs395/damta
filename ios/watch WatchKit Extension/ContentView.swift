e9717c4c3a069e5d351a2dfa72f46174a57ac086
shs395 <shs395@ajou.ac.kr>
2022-03-06 00:59:40 +0900

    watch extension build 가능

---
+++ ios/watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watch WatchKit Extension
+//
+//  Created by 신현석 on 2022/03/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/watch WatchKit Extension/RunnerApp.swift
@@ -0,0 +1,19 @@
+//
+//  RunnerApp.swift
+//  watch WatchKit Extension
+//
+//  Created by 신현석 on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct RunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
